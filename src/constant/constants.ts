fd455fb33101fa26c354615d4d74914ff9799b31
Devanps <devanps212@gmail.com>
2025-03-18 16:36:58 +0530

    constant files added for static rendering

---
+++ src/constant/constants.ts
@@ -0,0 +1,19 @@
+import { FAQ } from "./faq";
+import { CONSTESTS, COMMUNITY, PLATINUM, PRIME } from "./home/home";
+import {
+  ANIMATION_TEXTS_ADDA,
+  ANIMATION_TEXTS_HOME,
+} from "./common/animationText";
+import { NAV_LINKS, ADDA_NAV_LINKS } from ".";
+
+export {
+  FAQ,
+  CONSTESTS,
+  COMMUNITY,
+  PLATINUM,
+  PRIME,
+  ANIMATION_TEXTS_ADDA,
+  ANIMATION_TEXTS_HOME,
+  NAV_LINKS,
+  ADDA_NAV_LINKS,
+};
