2127e2efb06b7bbf43849759d9bd2eecd49d1ce0
Devanps <devanps212@gmail.com>
2025-06-20 10:14:30 +0530

    live notification completed

--- src/context/adda/provider/socketProvider.tsx
+++ src/context/adda/provider/socketProvider.tsx
@@ -1,23 +1,23 @@
-import { ReactNode, useEffect, useRef } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { io, Socket } from "socket.io-client";
 import { useAuth } from "@clerk/clerk-react";
 import SocketContext from "../socket";
 
 export const SocketProvider = ({ children }: { children: ReactNode }) => {
   const { getToken } = useAuth();
-  const socketRef = useRef<Socket | null>(null);
+  const [socket, setSocket] = useState<Socket | null>(null);
 
   useEffect(() => {
     const connectSocket = async () => {
       const token = await getToken();
 
-      const newSocket = io(import.meta.env.VITE_PROD_URL, {
+      const newSocket = io(import.meta.env.VITE_DEV_URL, {
         transports: ["websocket"],
         withCredentials: true,
         auth: { token },
       });
 
-      socketRef.current = newSocket;
+      setSocket(newSocket);
 
       newSocket.on("connect", () => {
         console.log("Socket connected:", newSocket.id);
@@ -27,16 +27,14 @@ export const SocketProvider = ({ children }: { children: ReactNode }) => {
     connectSocket();
 
     return () => {
-      if (socketRef.current) {
-        socketRef.current.disconnect();
+      if (socket) {
+        socket.disconnect();
         console.log("Socket disconnected");
       }
     };
   }, [getToken]);
 
   return (
-    <SocketContext.Provider value={socketRef.current}>
-      {children}
-    </SocketContext.Provider>
+    <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
   );
 };
